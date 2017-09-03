54eeb56df583fd73ab19698c684c0e129d0d60f1
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-09-04 00:12:15 +0200

    model schema started

--- server/server.js
+++ server/server.js
@@ -3,15 +3,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server,{serveClient:true});
+const mongoose = require('mongoose');
+const bluebird = require('blue')
 
-nunjucks.configure('./client/dist/',{
+mongoose.connect('mongodb://localhost:27017/chat');
+    mongoose.Promise = require('bluebird');
+
+nunjucks.configure('./client/views/',{
     autoescape: true,
     express: app
 });
+app.use('/assets',express.static('./client/public'));
 
 app.get('/',function(req,res) {
     res.render('index.html');
 });
-app.listen(7777,() =>  {
+io.on('connection',function(socket) {
+    socket.emit('connected',"You are connected");
+    socket.join('all');
+    socket.on('msg',content => {
+        const obj= {
+            date: new Date(),
+            content: content,
+            username: socket.id
+        }
+        socket.emit("message",obj);
+        socket.to('all').emit('message',obj);
+    })
+})
+server.listen(7777,() =>  {
     console.log('server started on port 7777');
 });
